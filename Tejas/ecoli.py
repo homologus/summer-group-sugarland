7153d339fb55a7f6c0cdb054585d490dd89c997d
Tejas Kalpathi <tejaskalpathi@gmail.com>
2020-06-10 17:11:28 +0000

    created a file

---
+++ Tejas/ecoli.py
@@ -0,0 +1,9 @@
+x = open("/share/Ecoli/GCA_000005845.2_ASM584v2_genomic.fna", "r")
+count = 0
+y = x.read()
+
+for i in range(69, len(y), 3):
+	if(y[i:i+3] ==  "AGC"):
+		count = count+1
+
+print(count)
